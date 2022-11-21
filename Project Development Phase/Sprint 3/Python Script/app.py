12cd8285b81e2893c982fcf007000ff1500c022c
Akshaya2304 <113496353+Akshaya2304@users.noreply.github.com>
2022-11-21 21:14:10 +0530

    Update app.py

--- Project Development Phase/Sprint 3/Python Script/app.py
+++ Project Development Phase/Sprint 3/Python Script/app.py
@@ -3,18 +3,13 @@ from flask_cors import cross_origin
 import pandas as pd
 import numpy as np
 import datetime
+import pickle
 
-import requests
 
-# NOTE: you must manually set API_KEY below using information retrieved from your IBM Cloud account.
-API_KEY = "dRe32OTFMluCZqk6qFtDz95jkqS8FbBK4TrAuw7TgqR4"
-token_response = requests.post('https://iam.cloud.ibm.com/identity/token', data={"apikey":
- API_KEY, "grant_type": 'urn:ibm:params:oauth:grant-type:apikey'})
-mltoken = token_response.json()["access_token"]
-
-header = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + mltoken}
 
 app = Flask(__name__, template_folder="template")
+models = pickle.load(open("./models/cat.pkl", "rb"))
+print("Model Loaded")
 
 @app.route("/",methods=['GET'])
 @cross_origin()
@@ -30,78 +25,59 @@ def predict():
 		day = float(pd.to_datetime(date, format="%Y-%m-%dT").day)
 		month = float(pd.to_datetime(date, format="%Y-%m-%dT").month)
 		# MinTemp
-		minTemp = (request.form['mintemp'])
+		minTemp = float(request.form['mintemp'])
 		# MaxTemp
-		maxTemp = (request.form['maxtemp'])
+		maxTemp = float(request.form['maxtemp'])
 		# Rainfall
-		rainfall = (request.form['rainfall'])
+		rainfall = float(request.form['rainfall'])
 		# Evaporation
-		evaporation = (request.form['evaporation'])
+		evaporation = float(request.form['evaporation'])
 		# Sunshine
-		sunshine = (request.form['sunshine'])
+		sunshine = float(request.form['sunshine'])
 		# Wind Gust Speed
-		windGustSpeed = (request.form['windgustspeed'])
+		windGustSpeed = float(request.form['windgustspeed'])
 		# Wind Speed 9am
-		windSpeed9am = (request.form['windspeed9am'])
+		windSpeed9am = float(request.form['windspeed9am'])
 		# Wind Speed 3pm
-		windSpeed3pm = (request.form['windspeed3pm'])
+		windSpeed3pm = float(request.form['windspeed3pm'])
 		# Humidity 9am
-		humidity9am =(request.form['humidity9am'])
+		humidity9am = float(request.form['humidity9am'])
 		# Humidity 3pm
-		humidity3pm = (request.form['humidity3pm'])
+		humidity3pm = float(request.form['humidity3pm'])
 		# Pressure 9am
-		pressure9am = (request.form['pressure9am'])
+		pressure9am = float(request.form['pressure9am'])
 		# Pressure 3pm
-		pressure3pm = (request.form['pressure3pm'])
+		pressure3pm = float(request.form['pressure3pm'])
 		# Temperature 9am
-		temp9am = (request.form['temp9am'])
+		temp9am = float(request.form['temp9am'])
 		# Temperature 3pm
-		temp3pm = (request.form['temp3pm'])
+		temp3pm = float(request.form['temp3pm'])
 		# Cloud 9am
-		cloud9am = (request.form['cloud9am'])
+		cloud9am = float(request.form['cloud9am'])
 		# Cloud 3pm
-		cloud3pm = (request.form['cloud3pm'])
+		cloud3pm = float(request.form['cloud3pm'])
 		# Cloud 3pm
-		location = (request.form['location'])
+		location = float(request.form['location'])
 		# Wind Dir 9am
-		winddDir9am = (request.form['winddir9am'])
+		winddDir9am = float(request.form['winddir9am'])
 		# Wind Dir 3pm
-		winddDir3pm = (request.form['winddir3pm'])
+		winddDir3pm = float(request.form['winddir3pm'])
 		# Wind Gust Dir
-		windGustDir = (request.form['windgustdir'])
+		windGustDir = float(request.form['windgustdir'])
 		# Rain Today
-		rainToday = (request.form['raintoday'])
+		rainToday = float(request.form['raintoday'])
+
+		input_lst = [location , minTemp , maxTemp , rainfall , evaporation , sunshine ,
+					 windGustDir , windGustSpeed , winddDir9am , winddDir3pm , windSpeed9am , windSpeed3pm ,
+					 humidity9am , humidity3pm , pressure9am , pressure3pm , cloud9am , cloud3pm , temp9am , temp3pm ,
+					 rainToday , month , day]
+		pred = models.predict(input_lst)
+		output = pred
+		if output == 0:
+			return render_template("after_sunny.html")
+		else:
+			return render_template("after_rainy.html")
 	return render_template("predictor.html")
-		
-		
-	
 
-# NOTE: manually define and pass the array(s) of values to be scored in the next line
-payload_scoring = {"input_data": [{"field": [[
-                                "f0",
-                                "f1",
-                                "f2",
-                                "f3",
-                                "f4",
-                                "f5",
-                                "f6",
-                                "f7",
-                                "f8",
-                                "f9",
-                                "f10",
-                                "f11",
-                                "f12",
-                                "f13",
-                                "f14",
-                                "f15",
-                                "f16"
-                        ]], "values":[[15, 25.5, 3.7, 4.2, 9.3, 35, 3, 15, 79, 37, 1012.5, 1008.2, 7, 4, 17.6, 27.7, 3.5]]}]}
-
-response_scoring = requests.post('https://us-south.ml.cloud.ibm.com/ml/v4/deployments/9f322f69-cea1-4ccb-9a5c-49ec65275698/predictions?version=2022-11-18', json=payload_scoring,
- headers={'Authorization': 'Bearer ' + mltoken})
-print("Scoring response")
-predict = response_scoring.json()
-val = (predict['predictions'][0]['values'][0][0])	
-print(val)
 if __name__=='__main__':
 	app.run(debug=True)
